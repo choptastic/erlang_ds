09b23d1d9e43b5938de5c21dc53d8ccb813515b6
Jesse Gumm <gumm@sigma-star.com>
2023-08-13 12:15:12 -0500

    Add type-specs, however, dialyzer still doesn't pass
    dialyzer may not pass because of the issues with `dict` not being
    reliably guardable

--- src/ds.erl
+++ src/ds.erl
@@ -38,9 +38,26 @@
 -define(IS_BLANK(V), (V==undefined orelse V=="" orelse V==<<>>)).
 -define(IS_PROPLIST(V), (is_list(V) andalso is_tuple(hd(V)) andalso tuple_size(hd(V))==2)).
 
+-type object() :: dict:dict() | list() | map().
+-type key() :: any().
+-type keys() :: [key()].
+-type value() :: any().
+-type values() :: [value()].
+-type default_value() :: value().
+-type key_value_tuple() :: {key(), value()}.
+-type key_value_tuples() :: [key_value_tuple()].
+-type transform_action() :: atom() | function().
+-type transform_tuple() :: {transform_action(), keys()}.
+-type transform_list() :: [transform_tuple()].
+-type key_map() :: [{key(), key()}].
+-type unmerged_object() :: object().
+-type proplist() :: [{key(), value()}].
+
+-spec set(object(), key(), value()) -> object().
 set(Obj,Key,Val) ->
     set(Obj,{Key,Val}).
 
+-spec set(object(), key_value_tuple() | key_value_tuples()) -> object().
 set(Obj,{Key,Val}) when is_list(Obj) ->
     % delete a matching key if exists and prepend {K,V} to the list. No guarantee of proplist order
     [{Key,Val} | delete(Obj,Key)];
@@ -55,13 +72,16 @@ set(Obj,[{Key,Val} | Rest]) ->
     set(NewObj,Rest).
 
 % Will get a list of values in the order specified in the "Keys" argument
+-spec get_list(object(), keys(), default_value()) -> values().
 get_list(Obj,Keys,Default) when is_list(Keys) ->
     [get(Obj,Key,Default) || Key<-Keys].
 
+-spec get_list(object(), keys()) -> values().
 get_list(Obj,Keys) when is_list(Keys) ->
     get_list(Obj,Keys,"").
 
 % Get a single value using the key "Key" and if not found, return "Default"
+-spec get(object(), key(), default_value()) -> value().
 get(Obj,Key,Default) when is_list(Obj) ->
     proplists:get_value(Key,Obj,Default);
 get(Obj,Key,Default) when is_map(Obj) ->
@@ -72,9 +92,11 @@ get(Obj,Key,Default) when ?IS_DICT(Obj) ->
         error -> Default
     end.
 
+-spec get(object(), key()) -> value().
 get(Obj,Key) ->
     get(Obj,Key,"").
 
+-spec has_key(object(), key()) -> boolean().
 has_key(Obj,Key) when is_list(Obj) ->
     lists:keymember(Key,1,Obj);
 has_key(Obj,Key) when is_map(Obj) ->
@@ -82,6 +104,7 @@ has_key(Obj,Key) when is_map(Obj) ->
 has_key(Obj,Key) when ?IS_DICT(Obj) ->
     dict:is_key(Key, Obj).
 
+-spec update(object(), key() | keys(), fun()) -> object().
 update(Obj,[],_FormatFun) ->
     Obj;
 update(Obj,[Key|RestKeys],FormatFun) ->
@@ -91,7 +114,7 @@ update(Obj,Key,FormatFun) ->
     NewVal = FormatFun(get(Obj,Key)),
     set(Obj,Key,NewVal).
 
-
+-spec boolize(object(), keys()) -> object().
 boolize(Obj,Keys) ->
     update(Obj,Keys,fun(V) ->
         if
@@ -107,6 +130,7 @@ boolize(Obj,Keys) ->
 %% DateFormat can be date, unixtime, or a binary/string formatted for use with qdate, or any other term registered as a format with qdate
 %% Requires qdate installed.
 %% If DateFormat cannot be handled, will instead 
+-spec format_date(object(), keys(), DateFormat :: unixtime | date | now | any()) -> object().
 format_date(Obj, Keys, DateFormat) ->
     UpdateFun = case DateFormat of
         unixtime -> fun(D) -> try qdate:to_unixtime(D) catch _:_ -> 0 end end;
@@ -116,6 +140,7 @@ format_date(Obj, Keys, DateFormat) ->
     end,
     update(Obj, Keys, UpdateFun).
 
+-spec atomize(object(), keys()) -> object().
 atomize(Obj,Keys) ->
     update(Obj,Keys,fun
         (V) when is_list(V) -> list_to_atom(V);
@@ -125,7 +150,7 @@ atomize(Obj,Keys) ->
         (V) -> throw({cannot_convert_to_atom,V})
     end).
 
-
+-spec transform(object(), transform_list()) -> object().
 transform(Obj,{date, Keys}) ->
     format_date(Obj, Keys, date);
 transform(Obj,{unixtime, Keys}) ->
@@ -149,6 +174,7 @@ transform(Obj,Map) when is_list(Map) ->
         transform(Acc,Action)
     end,Obj,Map).
 
+-spec map(object(), fun()) -> object().
 map(Obj,Fun) when is_list(Obj) andalso is_function(Fun, 1) ->
     [{Key,Fun(Val)} || {Key,Val} <- Obj];
 map(Obj,Fun) when is_list(Obj) andalso is_function(Fun, 2) ->
@@ -158,9 +184,11 @@ map(Obj,Fun) when is_map(Obj); ?IS_DICT(Obj)
                            orelse is_function(Fun, 2)) ->
     as_list(Obj, fun(List) -> map(List, Fun) end).
 
+-spec rekey(object(), FromKey :: key(), ToKey :: key()) -> object().
 rekey(Obj,FromKey,ToKey) ->
     rekey(Obj,[{FromKey,ToKey}]).
 
+-spec rekey(object(), key_map()) -> object().
 rekey(Obj,KeyMap) when is_list(Obj) ->
     lists:map(fun({K,V}) ->
         {rekey_swapkey(K,KeyMap),V}
@@ -177,12 +205,14 @@ rekey_swapkey(Current,KeyMap) ->
 
 % TODO: improve performance by only traversing main list once. Fast enough for now
 % Maybe just use filter/2.
+-spec delete_list(object(), keys()) -> object().
 delete_list(Obj, []) ->
     Obj;
 delete_list(Obj,[Key|RestKeys]) ->
     NewObj = delete(Obj,Key),
     delete_list(NewObj,RestKeys).
 
+-spec delete(object(), key()) -> object().
 delete(Obj,Key) when is_list(Obj) ->
     lists:keydelete(Key, 1, Obj);
 delete(Obj,Key) when is_map(Obj) ->
@@ -192,12 +222,14 @@ delete(Obj,Key) when ?IS_DICT(Obj) ->
 
 
 % Opposite of delete_list(): keeps all listed keys and removes all others
+-spec keep(object(), keys()) -> object().
 keep(Obj,Keys) ->
     FilterFun = fun(Key,_Val) ->
         lists:member(Key,Keys)
     end,
     filter(Obj, FilterFun).
 
+-spec filter(object(), fun()) -> object().
 filter(Obj, Fun) when is_list(Obj) ->
     %% maps and dict filters use fun(K,V), while lists:filter would use
     %% fun({K,V}), so we need a translation
@@ -208,7 +240,6 @@ filter(Obj, Fun) when is_map(Obj) ->
 filter(Obj, Fun) when ?IS_DICT(Obj) ->
     dict:filter(Fun, Obj).
     
-
 merge_full(ExtendedProplist) when ?IS_PROPLIST(ExtendedProplist) ->
     lists:foldl(fun
         ({_,Val},Acc) when ?IS_BLANK(Val) ->
@@ -237,12 +268,14 @@ merge_full(ExtendedProplist) when ?IS_PROPLIST(ExtendedProplist) ->
                     {Merged,NewUnmerged}
             end
     end,{[],[]},ExtendedProplist).
-    
+ 
+-spec merge(object(), object()) -> {object(), unmerged_object()}.
 merge(A,B) ->
     merge([A,B]).
     
 %% Returns a two_tuple: {Merged, Unmerged}
 %% Merged is the new merged result
+-spec merge([object()]) -> {object(), unmerged_object()}.
 merge(List) ->
     Type = type(hd(List)),
     List2 = lists:flatten([to_list(X) || X <- List]),
@@ -251,6 +284,7 @@ merge(List) ->
 
 %% Returns a single proplist with a guess of the final proplist.
 %% For each field, just returns the first value it encounters, even if it's not the same
+-spec guess_merge([object()]) -> object().
 guess_merge([]) ->
     [];
 guess_merge(List) when ?IS_PROPLIST(List) ->
@@ -269,6 +303,7 @@ guess_merge(Objs) ->
     Merged = guess_merge(FlatProplist),
     to_type(ToType, Merged).
 
+-spec guess_merge(object(), object()) -> object().
 guess_merge(A,B) ->
     guess_merge([A,B]).
 
@@ -335,13 +370,15 @@ compare(A,B,[SortField|RestSorts]) ->
 
 % this converts Obj to a proplist, runs the script as the proplist, and returns
 % it back to it's original type
-as_list(Obj, Fun) when is_list(Obj) ->
-    Fun(Obj);
+-spec as_list(object(), fun()) -> object().
+%as_list(Obj, Fun) when is_list(Obj) ->
+%    Fun(Obj);
 as_list(Obj, Fun) when is_map(Obj) ->
     from_and_to_map(Obj, Fun);
 as_list(Obj, Fun) when ?IS_DICT(Obj) ->
     from_and_to_dict(Obj, Fun).
 
+-spec to_list(object()) -> proplist().
 to_list(Obj) when is_list(Obj) ->
     Obj;
 to_list(Obj) when is_map(Obj) ->
@@ -349,6 +386,7 @@ to_list(Obj) when is_map(Obj) ->
 to_list(Obj) when ?IS_DICT(Obj) ->
     dict:to_list(Obj).
 
+-spec to_dict(object()) -> dict:dict().
 to_dict(Obj) when ?IS_DICT(Obj) ->
     Obj;
 to_dict(Obj) when is_list(Obj) ->
@@ -357,6 +395,7 @@ to_dict(Obj) when is_map(Obj) ->
     List = maps:to_list(Obj),
     dict:from_list(List).
 
+-spec to_map(object()) -> map().
 to_map(Obj) when is_list(Obj) ->
     maps:from_list(Obj);
 to_map(Obj) when is_map(Obj) ->
@@ -395,6 +434,7 @@ from_and_to_map(Obj, Fun) ->
     List2 = Fun(List),
     maps:from_list(List2).
 
+-spec type(object()) -> list | map | dict.
 type(Obj) when is_list(Obj) ->
     list;
 type(Obj) when is_map(Obj) ->
